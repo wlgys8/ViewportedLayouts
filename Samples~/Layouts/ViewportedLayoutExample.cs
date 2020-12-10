5ba8a7d5084a3ff13a812580960bb997f31e4191
zilch <wlgys8@gmail.com>
2020-12-10 15:12:50 +0800

    add Samples

---
+++ Samples~/Layouts/ViewportedLayoutExample.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MS.UGUI.ViewportedLayouts;
+using TMPro;
+using UnityEngine.UI;
+
+public class ViewportedLayoutExample : MonoBehaviour
+{
+    public ScrollRect linearScroll;
+    public SimpleFlexGridLayout simpleGridLayout;
+    public SimpleLinearLayout simpleLinearLayout;
+
+    void Start(){
+        simpleGridLayout.itemViewUpdateEvent += ((index)=>{
+            var view = simpleGridLayout.GetView(index);
+            view.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = index.ToString();
+        });
+
+        simpleLinearLayout.itemViewUpdateEvent += ((index)=>{
+            var view = simpleLinearLayout.GetView(index);
+            view.transform.Find("Text").GetComponent<TextMeshProUGUI>().text = index.ToString();
+        });
+
+        simpleLinearLayout.UpdateImmediately();
+        linearScroll.verticalNormalizedPosition = 1;
+    }
+}
